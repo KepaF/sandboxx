b8e21a0ebe2342cabc0f015b63169adaa696cbbe
Murashkina <grewytri33@gmail.com>
2020-10-04 21:04:31 +0400

    1.2 Добавлено 4 контруктора класса Person

--- src/main/java/ru/ssau/tk/_KEPAF_/_sandboxx_/Person.java
+++ src/main/java/ru/ssau/tk/_KEPAF_/_sandboxx_/Person.java
@@ -4,6 +4,24 @@ public class Person {
     private String firstName;
     private String lastName;
     private int passportId;
+    public Person() {
+
+    }
+
+    public Person(int passportId) {
+        this.passportId = passportId;
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Person(String firstName, String lastName, int passportId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.passportId = passportId;
+    }
     public String getFirstName(){
         return firstName;
     }
