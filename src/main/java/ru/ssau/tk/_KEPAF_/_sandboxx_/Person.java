ac632b850378c906e9f18c8ed2bc2ac61d3e8d6f
Murashkina <grewytri33@gmail.com>
2020-10-04 17:52:29 +0400

    1.1 Добавлены приватные поля firstName, secondName и passportId ,созданы геттеры и сеттеры для них.

--- src/main/java/ru/ssau/tk/_KEPAF_/_sandboxx_/Person.java
+++ src/main/java/ru/ssau/tk/_KEPAF_/_sandboxx_/Person.java
@@ -1,4 +1,26 @@
 package ru.ssau.tk._KEPAF_._sandboxx_;
 
 public class Person {
+    private String firstName;
+    private String lastName;
+    private int passportId;
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    int getPassportId(){
+        return passportId;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName=firstName;
+    }
+    public void setLastNameName(String lastName){
+        this.lastName=lastName;
+    }
+    public void setPassportId(int passportId){
+        this.passportId=passportId;
+    }
 }
