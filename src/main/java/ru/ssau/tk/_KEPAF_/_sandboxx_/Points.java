cadfc9ed8676d74b47a0cadedb07741a52ec5aa3
Murashkina <grewytri33@gmail.com>
2020-10-04 22:48:48 +0400

    1.5 Добавлен метод enlarge

--- src/main/java/ru/ssau/tk/_KEPAF_/_sandboxx_/Points.java
+++ src/main/java/ru/ssau/tk/_KEPAF_/_sandboxx_/Points.java
@@ -1,6 +1,8 @@
 package ru.ssau.tk._KEPAF_._sandboxx_;
 
 public class Points {
+    private Points(){};
+
     static Point sum(Point point1,Point point2){
         return new Point(point1.getX()+point2.getX(),point1.getY()+point2.getY(),point1.getZ()+point2.getZ());
     }
@@ -14,5 +16,9 @@ public class Points {
         return new Point(point1.getX()/point2.getX(),point1.getY()/point2.getY(),point1.getZ()/ point2.getZ());
     }
 
-    private Points(){};
+
+    public static Point enlarge(Point point, double number) {
+        return new Point(point.x * number, point.y * number, point.z * number);
+    }
+
 }
