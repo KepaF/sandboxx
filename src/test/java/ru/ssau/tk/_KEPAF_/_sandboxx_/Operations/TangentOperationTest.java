f906eea3b16234c8b8f0a5cec72d16fe9bf3bfa6
Murashkina <grewytri33@gmail.com>
2020-11-26 01:01:18 +0400

    1.15 классы IdenticalOperation,SqrtOperation и TangentOperation покрыты тестами

--- src/test/java/ru/ssau/tk/_KEPAF_/_sandboxx_/Operations/TangentOperationTest.java
+++ src/test/java/ru/ssau/tk/_KEPAF_/_sandboxx_/Operations/TangentOperationTest.java
@@ -10,7 +10,7 @@ public class TangentOperationTest {
 
     @Test
     public void testApply() {
-        assertEquals(a.apply(Math.PI/4), 1, DELTA);
+        assertEquals(a.apply(Math.PI / 4), 1, DELTA);
         assertEquals(a.apply(Double.NEGATIVE_INFINITY), Double.NaN, DELTA);
         assertEquals(a.apply(Double.POSITIVE_INFINITY), Double.NaN, DELTA);
         assertEquals(a.apply(Double.NaN), Double.NaN, DELTA);
@@ -18,7 +18,7 @@ public class TangentOperationTest {
 
     @Test
     public void testApplyTriple() {
-        assertEquals(a.applyTriple(Math.PI/4), Math.tan(Math.tan(Math.tan(Math.PI/4))), DELTA);
+        assertEquals(a.applyTriple(Math.PI / 4), Math.tan(Math.tan(Math.tan(Math.PI / 4))), DELTA);
         assertEquals(a.applyTriple(Double.NEGATIVE_INFINITY), Double.NaN, DELTA);
         assertEquals(a.applyTriple(Double.POSITIVE_INFINITY), Double.NaN, DELTA);
         assertEquals(a.applyTriple(Double.NaN), Double.NaN, DELTA);
