08e77a211b4fba99c85f235e1e5f95352e55b16a
Murashkina <grewytri33@gmail.com>
2020-12-23 20:51:37 +0400

    Добавлено задание 3_6

---
+++ src/main/java/ru/ssau/tk/_KEPAF_/_sandboxx_/String/String3_6.java
@@ -0,0 +1,16 @@
+package ru.ssau.tk._KEPAF_._sandboxx_.String;
+
+public class String3_6 {
+    public static void main(String[] args) {
+        int i = 0;
+        System.out.println("Символ\t№" + ++i);
+        System.out.println("Символ\b№" + ++i);
+        System.out.println("Символ\n№" + ++i);
+        System.out.println("Символ\r№" + ++i);
+        System.out.println("Символ\'№" + ++i);
+        System.out.println("Символ\"№" + ++i);
+        System.out.println("Символ\\№" + ++i);
+
+    }
+
+}
